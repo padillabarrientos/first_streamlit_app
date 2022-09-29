a51a7bb49df8033e7f0509c9f34b0420d52efe8a
Juan Ignacio Padilla <110518340+padillabarrientos@users.noreply.github.com>
2022-09-28 22:59:32 -0600

    Update streamlit_app.py
    Cambiamos el indice a Fruit
    Agregamos un multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselet('Pick some fruits' , list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
