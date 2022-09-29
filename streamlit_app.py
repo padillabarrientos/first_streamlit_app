d826fc33088cf4f2f121b245afae1b2c66d7ef53
Juan Ignacio Padilla <110518340+padillabarrientos@users.noreply.github.com>
2022-09-28 22:47:39 -0600

    Update streamlit_app.py
    emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Arrocito')
-streamlit.text('Texto1')
-
+streamlit.header(🥣'Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
+  streamlit.text(🥑🍞 'Avocado Toast')
