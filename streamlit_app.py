67cf2e293b11f187e9e05ff7fafbe66c068ec160
padillabarrientos <110518340+padillabarrientos@users.noreply.github.com>
2022-10-02 23:18:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect('Pick some fruits' , list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-
 streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
@@ -25,20 +22,21 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-streamlit.header('TheFruit load list contains:')
-
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-  return my_cur.fetchall()
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list;")
+    return my_cur.fetchall()
 
+    
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-  streamlit.text(my_data_rows)
     
+streamlit.header('TheFruit load list contains:')
 
+
+    
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
