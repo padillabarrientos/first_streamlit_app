9f90def962bdc2444d903dbfcf9d0cbc0a453f11
padillabarrientos <110518340+padillabarrientos@users.noreply.github.com>
2022-10-02 22:55:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     streamlit.text(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
@@ -37,7 +38,6 @@ except URLError as e:
 # write your own comment -what does the next line do? 
 
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Now')
 streamlit.write('The user entered now', fruit_choice2)
 
